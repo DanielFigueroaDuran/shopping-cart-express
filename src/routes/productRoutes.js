c55f94a9f2ab767e059fc89ee2a2bc71e8c407ac
DanielFigueroaDuran <figueroadurandanielelias@gmail.com>
2024-11-01 14:57:42 +0100

    adding productRoutes

--- src/routes/productRoutes.js
+++ src/routes/productRoutes.js
@@ -1,7 +1,19 @@
 import { Router } from "express";
-import { productController } from "../controllers/productController.js";
+import {
+      getProducts,
+      getProductId,
+      createProduct,
+      updateProduct,
+      deleteProduct
+} from "../controllers/productController.js";
 
 const router = Router();
+
+router.get('/', getProducts);
+router.get('/:id', getProductId);
+router.post('/', createProduct);
+router.put('/:id', updateProduct);
+router.delete('/:id', deleteProduct);
 
 
 export default router;

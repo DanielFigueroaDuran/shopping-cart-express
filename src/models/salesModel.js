1b16e4125b2b4a78f633e7838d12516da8492b5b
DanielFigueroaDuran <figueroadurandanielelias@gmail.com>
2024-11-02 20:54:38 +0100

    salesModel finalezed

--- src/models/salesModel.js
+++ src/models/salesModel.js
@@ -1,28 +1,18 @@
 import { pool } from "../config/db.js";
 
-//const table = products;
 
-export const showAllSales = async () => {
-      const { rows } = await pool.query(`SELECT * FROM products`);
-      return rows;
-};
-
-export const showSalesId = async (id) => {
-      const { rows } = await pool.query(`SELECT * FROM products where id =$1`, [id]);
+export const showSalesIdUsers = async (id) => {
+      const { rows } = await pool.query(`SELECT * FROM sales where id_users =$1`, [id]);
       return rows[0];
 };
 
-export const createSales = async (name, price, image) => {
-      const { rows } = await pool.query(`INSERT INTO products (name,price,image) VALUES ($1, $2, $3) RETURNING *`, [name, price, image]);
+export const createSales = async (id, state, total) => {
+      const { rows } = await pool.query(`INSERT INTO sales (id_users, state, total) VALUES ($1, $2, $3) RETURNING *`, [id, state, total]);
       return rows[0];
 };
 
-export const updateSales = async (name, price, image, id) => {
-      const { rows } = await pool.query(`UPDATE products SET name = $1, price = $2, image = $3 where id = $4 RETURNING *`, [name, price, image, id]);
+export const updateSalesStatus = async (state, id) => {
+      const { rows } = await pool.query(`UPDATE sales SET state = $1 where id_users = $2 RETURNING *`, [state, id]);
       return rows[0];
 };
 
-export const deleteSales = async (id) => {
-      const { rows } = await pool.query(`DELETE FROM products WHERE id = $1 RETURNING *`, [id]);
-      return rows[0];
-};

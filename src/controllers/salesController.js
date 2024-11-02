f7071bf8633b980ebbb1ea7bb54093bc595e5351
DanielFigueroaDuran <figueroadurandanielelias@gmail.com>
2024-11-02 22:03:46 +0100

    adding salesController

---
+++ src/controllers/salesController.js
@@ -0,0 +1,26 @@
+import {
+      createSales,
+      showSalesIdUsers
+} from "../models/salesModel.js";
+
+
+export const getSalesIdUsers = async (req, res, next) => {
+      const { id_users } = req.params;
+      try {
+            const response = await showSalesIdUsers(id_users);
+            res.status(200).json(response);
+      } catch (error) {
+            next(error);
+      }
+};
+
+export const createSales = async (req, res, next) => {
+      const { id_users, state, total } = req.body;
+      try {
+            const response = await createSales(id_users, state, total);
+            res.status(201).json(response);
+      } catch (error) {
+            next(error);
+      }
+};
+

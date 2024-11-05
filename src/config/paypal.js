476a5d328a34a8b83ee4acdbbab956968e949896
DanielFigueroaDuran <figueroadurandanielelias@gmail.com>
2024-11-05 21:57:47 +0100

    adding config/paypal

--- src/config/paypal.js
+++ src/config/paypal.js
@@ -1,9 +1,9 @@
-// const paypal = require("@paypal/checkout-server-sdk");
+import "dotenv/config.js"
 import paypal from "@paypal/checkout-server-sdk";
 // Configuración del entorno (Sandbox o Live)
 function environment() {
-      let clientId = "YOUR_CLIENT_ID";
-      let clientSecret = "YOUR_CLIENT_SECRET";
+      let clientId = process.env.PAYPAL_CLIENT_ID;
+      let clientSecret = process.env.PAYPAL_CLIENT_SECRET;
       return new paypal.core.SandboxEnvironment(clientId, clientSecret);
       // Para producción, usa paypal.core.LiveEnvironment
 }

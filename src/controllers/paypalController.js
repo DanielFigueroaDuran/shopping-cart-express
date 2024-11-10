f80dc04554752163cad14d933f41ccb22926fd27
DanielFigueroaDuran <figueroadurandanielelias@gmail.com>
2024-11-10 13:40:22 +0100

    adding controllers/paypalController

--- src/controllers/paypalController.js
+++ src/controllers/paypalController.js
@@ -1,5 +1,7 @@
 import { client } from "../config/paypal.js";
 
+// create order pay
+
 const createOrder = async (req, res) => {
       const request = new paypal.orders.OrdersCreateRequest();
       request.prefer("return=representation");
@@ -20,11 +22,25 @@ const createOrder = async (req, res) => {
       });
 
       try {
-            const response = await client().execute(request);
-            console.log(`Order ID: ${response.result.id}`);
-            return response.result;
+            const order = await client().execute(request);
+            res.json({ id: order.result.id }); // Enviar el ID de la orden al cliente
       } catch (err) {
-            console.error(err);
-            return null;
+            res.status(500).send(err);
       }
-}
+};
+
+//capture the order
+
+export const executePayment = async (req, res) => {
+      const { orderID } = req.body; // Recibe el orderID desde el frontend
+
+      const request = new paypal.orders.OrdersCaptureRequest(orderID);
+      request.requestBody({});
+
+      try {
+            const capture = await client().execute(request);
+            res.json(capture.result); // Enviar el resultado de la captura al cliente
+      } catch (err) {
+            res.status(500).send(err);
+      }
+};

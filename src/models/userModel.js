a0ce0c2330e4dfb382388eb699c74af3b5524c80
DanielFigueroaDuran <figueroadurandanielelias@gmail.com>
2024-11-12 20:44:35 +0100

    adding function showUserIdSupabase

--- src/models/userModel.js
+++ src/models/userModel.js
@@ -4,4 +4,9 @@ import { pool } from "../config/db.js"
 export const showRolesIdUser = async (idAuthSupabase) => {
       const { rows } = await pool.query("SELECT role FROM users where id_auth_supabase = $1", [idAuthSupabase]);
       return rows[0]?.role;
+};
+
+export const showUserIdSupabase = async (idAuthSupabase) => {
+      const { rows } = await pool.query("SELECT * FROM users where id_auth_supabase = $1", [idAuthSupabase]);
+      return rows[0];
 }
